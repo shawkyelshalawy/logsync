dc62de8228653cb8ef29065cf83d26bff50a0fd3
shawkyelshalawy <shawkyfariedelshalawy@gmail.com>
2024-12-10 15:47:29 +0200

    clusten membership and replication between the nodes is done

---
+++ internal/log/replicator.go
@@ -0,0 +1,143 @@
+package log
+
+import (
+	"context"
+	"sync"
+
+	api "github.com/shawlyelshalawy/logsync/api/v1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+type Replicator struct {
+	DialOptions []grpc.DialOption
+	LocalServer api.LogClient
+	logger *zap.Logger
+	mu sync.Mutex
+	servers map[string]chan struct{}
+	closed bool 
+	close chan struct{}
+}
+
+func (r *Replicator) Join(name, addr string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.init()
+
+	if r.closed {
+		return nil
+	}
+	if _, ok := r.servers[name]; ok {
+		return nil
+	}
+
+	r.servers[name] = make(chan struct{})
+	go r.replicate(addr, r.servers[name])
+	return nil
+}
+
+func (r *Replicator) replicate(addr string , leave chan struct{}) {
+	cc, err := grpc.NewClient(addr , r.DialOptions...)	
+	if err != nil {
+		r.logger.Error("failed to dial", zap.String("addr", addr), zap.Error(err))
+		return
+	}
+	defer cc.Close()
+
+	client := api.NewLogClient(cc)
+	ctx := context.Background()
+	stream, err := client.ConsumeStream(ctx, &api.ConsumeRequest{Offset: 0})
+
+	if err != nil {
+		r.logger.Error("failed to consume", zap.String("addr", addr), zap.Error(err))
+		return
+	}
+	 records := make(chan *api.Record)
+	go func ()  {
+		for {
+			recv , err := stream.Recv()
+			if err != nil {
+				r.logger.Error("failed to receive", zap.String("addr", addr), zap.Error(err))
+				return
+			}
+			records <- recv.Record
+		}
+	}()
+
+	for {
+		select {
+		case <-r.close:
+			return
+		case <-leave:
+			return
+		case record := <-records:
+			_, err := r.LocalServer.Produce(ctx, &api.ProduceRequest{Record: record})
+			if err != nil {
+				r.logger.Error("failed to produce", zap.String("addr", addr), zap.Error(err))
+				return
+			}
+
+		}
+	}
+}
+
+
+func (r *Replicator) Leave(name string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.init()
+
+	if _, ok := r.servers[name]; !ok {
+		return nil
+	}
+
+	close(r.servers[name])
+	delete(r.servers, name)
+	return nil
+
+}
+
+
+func (r *Replicator) init() {
+	if r.logger == nil {
+		r.logger = zap.L().Named("replicator")
+	}
+
+	if r.servers == nil {
+		r.servers = make(map[string]chan struct{})
+	}
+
+	if r.close == nil {
+		r.close = make(chan struct{})
+	}
+
+}
+
+
+
+func (r *Replicator) Close() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.init()
+
+	if r.closed {
+		return nil
+	}
+	r.closed = true
+	close(r.close)
+
+	return nil
+}
+
+
+
+func  (r *Replicator) logError(err error , msg, addr string) {
+	r.logger.Error(
+		msg,
+		zap.String("addr", addr),
+		zap.Error(err),
+	)
+}
+
--- internal/server/server.go
+++ internal/server/server.go
@@ -140,9 +140,7 @@ func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
 	return &api.ConsumeResponse{Record: record}, nil
 }
 
-func (s *grpcServer) ProduceStream(
-	stream api.Log_ProduceStreamServer,
-) error {
+func (s *grpcServer) ProduceStream( stream api.Log_ProduceStreamServer, ) error {
 	for {
 		req, err := stream.Recv()
 		if err != nil {
